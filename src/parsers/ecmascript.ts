d6167fb2dfaf95b6b9c4f81c116a822534df4216
yzydeveloper <wqrdw@qq.com>
2021-10-25 19:24:47 +0800

    feat(build): 测试babel

--- src/parsers/ecmascript.ts
+++ src/parsers/ecmascript.ts
@@ -1,4 +1,14 @@
+import type { NodePath } from '@babel/traverse'
 import { workspace } from 'vscode'
+import {
+    expressionStatement,
+    assignmentExpression,
+    memberExpression,
+    identifier,
+    objectExpression
+} from '@babel/types'
+import { parse } from '@babel/parser'
+import traverse from '@babel/traverse'
 export class EcmascriptParser {
     constructor(
         public readonly id: 'js' | 'ts' = 'js'
@@ -8,6 +18,23 @@ export class EcmascriptParser {
         const regex = /export default/
         const document = await workspace.openTextDocument(filepath)
         const texts = await document.getText()
+        const ast = parse(texts, {
+            sourceType: 'module'
+        })
+        traverse(ast, {
+            ExportDefaultDeclaration(path: NodePath) {
+                path.replaceWith(
+                    expressionStatement(
+                        assignmentExpression(
+                            '=',
+                            memberExpression(identifier('Identifier'), identifier('exports')),
+                            objectExpression(path.node.declaration.properties),
+                        )
+                    )
+                )
+            }
+        })
+        console.log(ast, 'ast')
         if (!texts) return {}
         // eslint-disable-next-line no-eval
         return (0, eval)(`(${texts.replace(regex, '')})`)
