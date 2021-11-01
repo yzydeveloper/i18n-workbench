28a4e73d961f297688ffefc6a8568c4643d9333f
yzydeveloper <wqrdw@qq.com>
2021-11-02 00:00:12 +0800

    feat(Extractor): 使用正则匹配源语言

--- src/extractor/Sfc.ts
+++ src/extractor/Sfc.ts
@@ -1,12 +1,22 @@
 import ExtractorAbstract, { ExtractorOptions, ExtractorResult } from './base'
 import { workspace } from 'vscode'
+import { parse } from '@vue/compiler-sfc'
 export class SfcExtractor extends ExtractorAbstract {
     // public readonly id = 'vue'
 
     async extractor(options: ExtractorOptions): Promise<ExtractorResult> {
         const doc = await workspace.openTextDocument(this.uri)
-        console.log(doc, 'doc')
-
+        const text = doc.getText()
+        const { descriptor } = parse(text)
+        const { template, script } = descriptor
+        console.log(template?.content, 'template')
+        console.log(script?.content, 'script')
+        console.log(template, 'template')
+        console.log(script, 'script')
+        const tagContent = template?.content.match(/(>)([^><]*[\u4E00-\u9FA5]+[^><]*)(<)/gm)
+        const tagAttr = template?.content.match(/(<[^\/\s]+)([^<>]+)(\/?>)/gm)
+        console.log(tagContent, 'tagContent')
+        console.log(tagAttr, 'tagAttr')
         console.log(options, 'options')
         return {
             id: 'vue'
