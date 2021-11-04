fb418285659603930dc22515704b4af950901235
yzydeveloper <wqrdw@qq.com>
2021-11-04 19:39:22 +0800

    feat(Sfc): 下班提交一下

--- src/extractor/Sfc.ts
+++ src/extractor/Sfc.ts
@@ -1,17 +1,37 @@
 import ExtractorAbstract, { ExtractorOptions, ExtractorResult } from './base'
 import { workspace } from 'vscode'
-import { baseParse } from '@vue/compiler-core'
+import {
+    // TemplateChildNode,
+    ElementNode,
+    // RootNode,
+    baseParse as parse,
+    //  traverseNode,
+    //  createTransformContext,
+    //   getBaseTransformPreset
+} from '@vue/compiler-core'
 // import { parse } from '@vue/compiler-sfc'
 export class SfcExtractor extends ExtractorAbstract {
     // public readonly id = 'vue'
 
     async extractor(options: ExtractorOptions): Promise<ExtractorResult> {
         const doc = await workspace.openTextDocument(this.uri)
-        const text = doc.getText()
-
+        const texts = doc.getText()
         // @vue/compiler-core
-        const ast = baseParse(text)
-        console.log(ast, 'ast')
+        const ast = parse(texts)
+        ast.children.forEach(node => {
+            if (node.type !== 1) // NODETYPES.ELEMENT
+                return
+
+            switch (node.tag) {
+                case 'template':
+                    this.parseTemplateText(node)
+                    break
+                case 'script':
+                    break
+                default:
+                    break
+            }
+        })
 
         // @vue/compiler-sfc
         // const { descriptor } = parse(text)
@@ -27,5 +47,36 @@ export class SfcExtractor extends ExtractorAbstract {
         return {
             id: 'vue'
         }
+    }
+
+    parseTemplateText(context: ElementNode) {
+        console.log(context, 'context')
+        console.log(context, 'context')
+        const loop = (node: ElementNode) => {
+            // 5-InterpolationNode
+            // 2-TEXTNODE
+            for (let i = 0; i < node.children.length; i++) {
+                const _node = node.children[i] as ElementNode
+                if (_node.children.length) {
+
+                }
+            }
+        }
+        loop(context)
+        // const [nodeTransforms, directiveTransforms] = getBaseTransformPreset()
+        // const context = createTransformContext(ast, {
+        //     ...directiveTransforms,
+        //     nodeTransforms: [
+        //         ...nodeTransforms,
+        //         (node) => {
+        //             console.log(node, 'node')
+        //         }
+        //     ],
+        // })
+        // traverseNode(ast, context)
+    }
+
+    parseJsText() {
+
     }
 }
